69d98dd4e114b22c60148e05de82ff46a1e60961
Django_hp820 <siabdel58@gmail.com>
2024-10-03 23:15:21 +0200

    change API endpoints 'Events' to Serializers.ReservationsSerializes  & Calendar url  events api:http://localhost:8000/service/api/reservations/

--- services_menage/serializers.py
+++ services_menage/serializers.py
@@ -3,7 +3,6 @@ from rest_framework import serializers
 from .models import Employee, Reservation, MaintenanceTask
 from schedule.models import Calendar, Event
 from django_celery_beat.models import PeriodicTask
-
 class CustomPeriodicTaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = PeriodicTask
@@ -22,18 +21,7 @@ class EmployeeSerializer(serializers.ModelSerializer):
         model = Employee
         fields = ['id', 'name', 'calendar']
 
-class ReservationSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Reservation
-        fields = ['id', 'client', 'check_in', 'check_out']
 
-class MaintenanceTaskSerializer(serializers.ModelSerializer):
-    reservation = ReservationSerializer(read_only=True)
-    employee = EmployeeSerializer(read_only=True)
-
-    class Meta:
-        model = MaintenanceTask
-        fields = ['id', 'reservation', 'employee', 'scheduled_time']
 
 class EventSerializer(serializers.ModelSerializer):
     
@@ -59,3 +47,58 @@ class PeriodicTaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = PeriodicTask
         fields = '__all__'
+
+ 
+# construire une API pour ceer des events pour Calendar
+class ReservationSerializer(serializers.ModelSerializer):
+    ##id = serializers.CharField(source='pk')
+    title = serializers.SerializerMethodField()
+    #start = serializers.DateTimeField(source='check_in')
+    #end = serializers.DateTimeField(source='check_out')
+
+    start = serializers.SerializerMethodField()  
+    end = serializers.SerializerMethodField()
+    ## bgColor = serializers.CharField(default='#00a9ff')
+    ## color = serializers.CharField(default='white')
+    
+    def get_start(self, obj):
+        return obj.check_in.isoformat()
+
+    def get_end(self, obj):
+        return obj.check_out.isoformat()
+
+    def get_title(self, obj):
+        return f"Reservation for {obj.property.name}"
+
+    def update(self, instance, validated_data):
+        instance.check_in = validated_data.get('check_in', instance.check_in)
+        instance.check_out = validated_data.get('check_out', instance.check_out)
+        instance.guest_name = validated_data.get('guest_name', instance.guest_name)
+        # Mettez à jour d'autres champs si nécessaire
+        instance.save()
+        return instance
+    
+    def validate(self, data):
+        if data['check_in'] >= data['check_out']:
+            raise serializers.ValidationError("End date must be after start date")
+        return data
+
+ 
+    class Meta:
+        model = Reservation
+        fields = [ 'id', 'title', 'start', 'end', 'guest_name', 'guest_email', 
+                  'check_in', 'check_out', 'property',  
+                  'reservation_status', 'number_of_guests', 'total_price',
+                  ]
+        extra_kwargs = {
+            'property': {'required': False}  # Rend le champ 'property' optionnel pour les mises à jour
+        }
+
+        
+class MaintenanceTaskSerializer(serializers.ModelSerializer):
+    reservation = ReservationSerializer(read_only=True)
+    employee = EmployeeSerializer(read_only=True)
+
+    class Meta:
+        model = MaintenanceTask
+        fields = ['id', 'reservation', 'employee', 'scheduled_time']
