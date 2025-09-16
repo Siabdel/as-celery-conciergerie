eeb7f602a2ebef284ee33253f65c81ac5ab6c466
Django_hp820 <siabdel58@gmail.com>
2025-09-16 13:19:31 +0200

    modifie start_docker.sh

--- start_docker.sh
+++ start_docker.sh
@@ -1,4 +1,7 @@
+## build docker 
 sudo docker-compose -f docker-compose.prod.yml up -d --build
+## se connecter au docker 
 sudo docker-compose -f docker-compose.prod.yml exec web  bash
-sudo docker-compose -f docker-compose.prod.yml up -d --build
-sudo docker-compose -f docker-compose.prod.yml exec web  bash
+sudo docker-compose -f docker-compose.prod.yml exec db   bash
+## executer une commande
+docker exec -i conciergerie_db ls -l  -U postgres 
